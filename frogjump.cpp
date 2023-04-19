7186eb90737ad781132f49e68b78f44afc792ee5
bhumii-ii <99078842+bhumii-ii@users.noreply.github.com>
2023-04-19 14:45:00 +0530

    sec commit

--- frogjump.cpp
+++ frogjump.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int min_energy(int index,int arr)
+int min_energy(int index,int arr[],int dp[])
 {
 
     if (index==0)
     {
         return 0;
     }
-    int left = min_energy(index-1)+ abs(arr[index]-arr[index-1]);
-    int right = min_energy(index-2)+ abs(arr[index]-arr[index-2]);
+    if(dp[index]!=-1)
+    {
+        return dp[index];
+    }
+    int left = min_energy(index-1,arr,dp)+ abs(arr[index]-arr[index-1]);
+    int right = min_energy(index-2,arr,dp)+ abs(arr[index]-arr[index-2]);
 
-    return min (left,right);
+    return dp[index]= min (left,right);
+    }
+    int main()
+    {
+        int arr[5]={10,11,34,56,20};
+        int dp[6]={-1,-1,-1,-1,-1,-1};
+        int min= min_energy(4,arr,dp);
+        cout<<min;
+        return 0;
     }
